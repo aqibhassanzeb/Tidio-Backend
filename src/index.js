a312fa1e7b1ac6244eae684390c9c7543b09254f
Aqib Hassan Zeb <aqibhassanzeb@gmail.com>
2022-10-21 16:44:49 +0500

    chat integrate

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import  userAuth  from './routes/user-routes.js';
 import  chatRoutes  from './routes/chat-routes.js';
 import  messages  from './routes/messages-routes.js';
 
+import {Server} from "socket.io"
 
 const app = express();
 dotenv.config();
@@ -45,6 +46,51 @@ app.use('*', (req, res) => {
 
 //Port
 const port = process.env.PORT || 3333;
-app.listen(port, () => {
+ const nodeServer= app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
+
+
+// socket.io portion 
+const io = new Server(nodeServer, {
+    pingTimeout: 60000,
+    cors: {
+      origin: process.env.LOCAL_LINK,
+      // credentials: true,
+    },
+  });
+
+
+
+  io.on("connection", (socket) => {
+    console.log("Connected to socket.io");
+    socket.on("setup",(userData)=>{
+        socket.join(userData._id)
+        console.log(userData._id)
+        socket.emit("connected")
+    })
+
+
+    socket.on("typing", (room) => {
+        socket.in(room).emit("typing")});
+    socket.on("stop typing", (room) =>{
+        socket.in(room).emit("stop typing")});
+    
+
+    socket.on("join chat",(room)=>{
+        socket.join(room)
+    })
+
+
+    socket.on("new message", (newMessageRecieved) => {
+        var chat = newMessageRecieved.chat;
+        if (!chat.users) return console.log("chat.users not defined");
+        
+        chat.users.forEach((user) => {
+            if (user._id == newMessageRecieved.sender._id) return;
+          socket.in(user._id).emit("message recieved", newMessageRecieved);
+        });
+      });
+
+
+    })
